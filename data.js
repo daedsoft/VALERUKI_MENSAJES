9b20ba1ee3841fa0a75f820932ca29117972aa3c
DIEGO ESCOBAR <you@example.com>
2022-11-22 12:16:52 -0500

    update

--- data.js
+++ data.js
@@ -1,8 +1,13 @@
 let messages = [
-    {'id': 1, 
-    'categoria' :'Cumpleaños', 
-    'mensaje' : '¡Feliz cumpleaños! Que en este día tan especial, Dios te bendiga con mucha felicidad. Que en la vida siempre encuentres razones para sonreír y dar gracias.'},
-    {'id': 2, 
-    'categoria' :'Familiares', 
-    'mensaje' : 'Desde que naciste eres dueña de una parte de mi corazón, te amo y admiro por la mujer valiosa y soñadora en la que te has convertido… el mundo se ve tan pequeño para ti mi hermana del alma.'},
+    {'id': 1, 'categoria':'Cumpleaños', 'mensaje':'¡Feliz cumpleaños! Que en este día tan especial, Dios te bendiga con mucha felicidad. Que en la vida siempre encuentres razones para sonreír y dar gracias.'},
+    {'id': 2, 'categoria':'Familiares', 'mensaje':'Desde que naciste eres dueña de una parte de mi corazón, te amo y admiro por la mujer valiosa y soñadora en la que te has convertido… el mundo se ve tan pequeño para ti mi hermana del alma.'},
+    {'id': 3, 'categoria':'Familiares', 'mensaje':'Pienso que se quedaron cortos al hablarme de tus dones y virtudes,  eres maravillosa y queremos darte las gracias por tus atenciones y ser la mejor anfitriona, te esperamos en casa.'},
+    {'id': 4, 'categoria':'Cumpleaños', 'mensaje':'Deseo que el amor y la alegría estén de tu lado en todos los instantes de tu vida. Eres una persona muy especial y considero que soy bendecida por contar con tu amistad y con tu cariño.'},
+    {'id': 5, 'categoria':'Cumpleaños', 'mensaje':'Lo mejor para ti en este día, que sigas cumpliendo muchos años más y llenando de alegría los corazones de todos.'},
+    {'id': 6, 'categoria':'Amor', 'mensaje':'Es fácil amar a alguien cuando todo es perfecto, mantener ese amor, durante los momentos dificiles, es lo que lo hace  incondicional. Te amo'},
+    {'id': 7, 'categoria':'Cumpleaños', 'mensaje':'¡Feliz cumpleaños! Que el Señor sea tu fuerza y vigor todos los días que te dé en esta tierra. Gracias por ser tan especial ♥'},
+    {'id': 8, 'categoria':'Amor', 'mensaje':'Te miro, sonrió y siempre me preguntó... ¿Qué es eso que haces para gustarme tanto?'},
+    {'id': 9, 'categoria':'Cumpleaños', 'mensaje':'Mis mejores deseos para tí, que el Señor te bendiga con mucha salud y prosperidad para ti y tu familia. Continua siendo esa mujer luchadora, amorosa, sencilla y amable.'},    
 ]
+
+// {'id': 10, 'categoria':'', 'mensaje':''}    
