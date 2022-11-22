5533068a28fc3e929f72bebbca5d647c65598aa6
DIEGO ESCOBAR <you@example.com>
2022-11-22 10:39:33 -0500

    start

---
+++ data.js
@@ -0,0 +1,8 @@
+let messages = [
+    {'id': 1, 
+    'categoria' :'cumple', 
+    'mensaje' : '¡Feliz cumpleaños! Que en este día tan especial, Dios te bendiga con mucha felicidad. Que en la vida siempre encuentres razones para sonreír y dar gracias.'},
+    {'id': 2, 
+    'categoria' :'familiar', 
+    'mensaje' : 'Desde que naciste eres dueña de una parte de mi corazón, te amo y admiro por la mujer valiosa y soñadora en la que te has convertido… el mundo se ve tan pequeño para ti mi hermana del alma.'},
+]
